21f2dad330c182bf19c37ef311db1a77d240a13e
rakiraki829613-a11y <rakiraki829613@gmail.com>
2026-05-28 12:29:53 +0530

    this is selfproduct problen

--- buy_and_sell_the_stock.py
+++ buy_and_sell_the_stock.py
@@ -1,9 +1,15 @@
-def duplicates(nums):
-    st=set()     
-    for num in nums:
-        if num in st:
-            return True
-        st.add(num)
-    return False
-nums={1,2,1,21,2}
-print(duplicates(nums))
+def productself(nums):
+    n=len(nums)
+    answer=[1]*n
+    prifix=1
+    for i in range(n):
+        answer[i]=prifix
+        prifix*=nums[i]
+
+    sufix=1
+    for i in range(n-1,-1,-1):
+        answer[i]=sufix
+        sufix*=nums[i]
+    return answer
+nums=[1,2,3,4]
+print(productself(nums))
