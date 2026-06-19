9a4642bc5e55cbbcabe737b02b8a4764bb15eac4
rakiraki829613-a11y <rakiraki829613@gmail.com>
2026-06-19 15:27:51 +0530

    using the hash map

---
+++ find_first_uniqe_charaters.py
@@ -0,0 +1,11 @@
+def unidefgh(s):
+    freq={}
+    for ch in s:
+        freq[ch]=freq.get(ch,0)+1
+
+    for i in range(len(s)):
+        freq[s[i]]==1
+        return i
+    return -1
+s="leetcode"
+print(unidefgh(s))    
